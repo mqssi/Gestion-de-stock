1579d11c18cf467216e9920d445929f512049035
mqssi <43760122+mqssi@users.noreply.github.com>
2020-04-15 13:45:07 +0200

    Excel

--- PL/USER_Liste_Produit.cs
+++ PL/USER_Liste_Produit.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using Microsoft.Reporting.WinForms;
+using Microsoft.Office.Interop.Excel;
 
 namespace GestionDeStock.PL
 {
@@ -383,6 +384,55 @@ namespace GestionDeStock.PL
                 MessageBox.Show(EX.Message);
 
             }
+
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog SFD = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx", ValidateNames = true })  //Filtrer seulement les fichiers Excel
+            {
+
+                if (SFD.ShowDialog() == DialogResult.OK)
+                {
+                    Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                    Workbook wb = app.Workbooks.Add(XlSheetType.xlWorksheet);
+                    Worksheet ws = (Worksheet)app.ActiveSheet;
+                    app.Visible = false;
+                    //Ajoute les lignes du fichier excel
+                    ws.Cells[1, 1] = "Id Produit";
+                    ws.Cells[1, 2] = "Nom Produit";
+                    ws.Cells[1, 3] = "Quantité";
+                    ws.Cells[1, 4] = "Prix";
+                    //ajouter liste de produit de base de donnée dans ficher excel
+                    var ListeProduit = db.Produits.ToList(); //Liste des produits
+                    int i = 2;
+                    foreach (var L in ListeProduit)
+                    {
+                        ws.Cells[i, 1] = L.ID_PRODUIT;
+                        ws.Cells[i, 2] = L.Nom_Produit;
+                        ws.Cells[i, 3] = L.Quantite_Produit;
+                        ws.Cells[i, 4] = L.Prix_Produit;
+                        i++;
+
+                    }
+
+                    //changer style de fichier-----------------------------
+                    ws.Range["A1:D1"].Interior.Color = Color.Teal; /// back ground color
+                    ws.Range["A1:D1"].Font.Color = Color.White; //Text color
+                    ws.Range["A1:D1"].Font.Size =15; //Size
+
+                    //centrer le text
+
+                    ws.Range["A:D"].HorizontalAlignment = XlHAlign.xlHAlignCenter;
+                    ws.Range["A1:D1"].ColumnWidth = 16;
+
+                    //--------------------------------
+                    wb.SaveAs(SFD.FileName); //Save dans fichier excel
+                    app.Quit();
+                    MessageBox.Show("Sauvegarde Excel effectuée", "Excel", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+            }
+
 
         }
     }
